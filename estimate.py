d4c6e378665f131885a679d3ec02fc608ab08b62
Kommerla <bharatchandrakommerla01.cb@gmail.com>
2021-06-05 22:51:24 +0530

    added solution

--- estimate.py
+++ estimate.py
@@ -1,3 +1,36 @@
+import math
+import unittest
+
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+            
+    def test_high_iters(self):
+        for i in range(500, 600):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
+class TestMC(unittest.TestCase):
+    def test_randomness(self):
+        pi0 = monte_carlo(15000)
+        pi1 = monte_carlo(15000)
+        
+        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+
+        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+
+    def test_accuracy(self):
+        for i in range(500, 600):
+            pi = monte_carlo(i)
+            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+        
+    
+if __name__ == "__main__":
+    unittest.main()
+
 #(1)Estimation of pi using walli formula
 def wallis(n, t=1.0):  #defining wallis function
     for i in range(1,n):
@@ -9,9 +42,7 @@ def wallis(n, t=1.0):  #defining wallis function
     return pi
 print(wallis(1000000))
 
-
 #(2)Estimation of pi using monte carlo simualtion
-import random       #import random library
 def monte_carlo(INTERVAL):   #defining monte_carlo() function
     for i in range(INTERVAL**2):
         points_on_circle = 0
